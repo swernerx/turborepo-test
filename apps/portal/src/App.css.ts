754c51b12164babddbe083a89ae062f3283390d4
Sebastian Werner <s.werner@sebastian-software.de>
2022-03-24 14:08:39 +0100

    Changed over CSS file to vanillaextract

---
+++ apps/portal/src/App.css.ts
@@ -0,0 +1,21 @@
+import { style } from '@vanilla-extract/css';
+
+export const AppClass = style({
+  textAlign: "center"
+})
+
+export const AppLogoClass = style({
+  height: "40vmin",
+  pointerEvents: "none"
+})
+
+export const AppHeaderClass = style({
+  backgroundColor: "#282c34",
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "calc(10px + 2vmin)",
+  color: "white"
+})
