a842f7620309e5e60d7324d0bacddfcd3fd1dfbc
Sebastian Werner <s.werner@sebastian-software.de>
2022-03-25 12:29:54 +0100

    Add support for VanillaExtract to ui lib

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,3 +1,5 @@
+const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -11,5 +13,16 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "webpack5"
+  },
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Add support for VanillaExtract
+    config.plugins.push(new VanillaExtractPlugin())
+
+    // Return the altered config
+    return config;
   }
 }
--- packages/ui/src/stories/Button.stories.tsx
+++ packages/ui/src/stories/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import { Button } from '../Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -20,22 +20,10 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
+  children: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+  children: 'Button',
 };
