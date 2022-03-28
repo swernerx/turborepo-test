2e594dd0185f86231c024fe1323cc42526757e67
Sebastian Werner <s.werner@sebastian-software.de>
2022-03-28 10:57:52 +0200

    Cleanup and synced start/build storybook wrappers

--- packages/storybook-config/bin/build-storybook.js
+++ packages/storybook-config/bin/build-storybook.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const { symlinkSync } = require("fs");
-
 const execa = require("execa");
 
 const configFolder = path.resolve(__dirname, "../config");
@@ -8,24 +7,14 @@ const binPath = path.resolve(__dirname, "../node_modules/.bin/build-storybook");
 
 (async () => {
   const params = ["--config-dir", configFolder, "--quiet", "-o", "public"];
-  console.log("Call:",binPath)
-  console.log("Storybook:", params);
 
   try {
-    console.log("XXX1")
-    const cmd = execa(binPath, params, {
-      preferLocal: true,
-    });
-    console.log("XXX2")
+    const cmd = execa(binPath, params);
     cmd.stdout.pipe(process.stdout);
     cmd.stderr.pipe(process.stdout);
-    console.log("XXX3")
     await cmd
-    console.log("XXX4")
   } catch (err) {
-    console.log("ERROR:",err)
+    console.log("Storybook Error:",err)
     process.exitCode = 1;
   }
-
-  console.log("done");
 })();
---
+++ packages/storybook-config/bin/start-storybook.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const { symlinkSync } = require("fs");
+const execa = require("execa");
+
+const configFolder = path.resolve(__dirname, "../config");
+const binPath = path.resolve(__dirname, "../node_modules/.bin/start-storybook");
+
+(async () => {
+  const params = ["--config-dir", configFolder, "--quiet", "-o", "public"];
+
+  try {
+    const cmd = execa(binPath, params);
+    cmd.stdout.pipe(process.stdout);
+    cmd.stderr.pipe(process.stdout);
+    await cmd
+  } catch (err) {
+    console.log("Storybook Error:",err)
+    process.exitCode = 1;
+  }
+})();
